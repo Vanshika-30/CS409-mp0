7f69662e6804649b70443ed1813c4f390d11374f
Vanshika-30 <jvanshika30@gmail.com>
2025-09-03 20:21:26 -0500

    Created profile card for vj19 and added CSS and JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,25 @@
-/* Your JS here. */
-console.log('Hello World!')
+const nameEl = document.getElementById("name");
+const imgEl = document.getElementById("profileImg");
+const bioEl = document.getElementById("bio");
+const cardEl = document.getElementById("profileCard");
+
+nameEl.addEventListener("click", () => {
+    bioEl.style.display = "none";
+  });
+  
+// Show bio when clicking on the image
+imgEl.addEventListener("click", () => {
+bioEl.style.display = "block";
+});
+
+document.getElementById("redBtn").addEventListener("click", () => {
+  cardEl.style.backgroundColor = "red";
+});
+
+document.getElementById("blueBtn").addEventListener("click", () => {
+  cardEl.style.backgroundColor = "blue";
+});
+
+document.getElementById("greenBtn").addEventListener("click", () => {
+  cardEl.style.backgroundColor = "green";
+});
